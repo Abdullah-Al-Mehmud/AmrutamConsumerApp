96ee1f14158a86d329461671de0265144cda43e5
mehmud <timehmud121@gmail.com>
2024-06-05 17:16:52 +0600

    added some responsiveness

--- src/pages/Questionnaire/QuestionnaireHero.jsx
+++ src/pages/Questionnaire/QuestionnaireHero.jsx
@@ -3,17 +3,17 @@ const QuestionnaireHero = () => {
   return (
     <div>
       <div className="h-52  bg-[url('./assets/QuestionnaireBg.png')] bg-cover bg-center bg-no-repeat">
-        <div className="flex  justify-center items-center text-center h-full">
+        <div className="flex flex-col md:flex-row justify-center items-center text-center h-full">
           <div className=" pt-6">
             <h1 className="text-center font-medium md:text-2xl">
               Search Topics
             </h1>
-            <div className="relative px-4">
+            <div className="relative px-4  ">
               <IoArrowForwardSharp className="absolute right-7 md:top-2/3 top-6 text-lg transform -translate-y-1/2 bg-white w-5" />
               <input
                 type="text"
                 placeholder="Search eg. Dosha Quiz, Mental Health etc"
-                className="md:mt-4 mt-2 lg:w-96 px-5 py-[8px] rounded-lg md:text-sm text-xs outline-none border-[1.5px] border-[#cdcdcd]"
+                className="md:mt-4 mt-2 lg:w-96 w-72 px-5 py-[8px] rounded-lg md:text-sm text-xs outline-none border-[1.5px] border-[#cdcdcd]"
               />
             </div>
           </div>
@@ -26,7 +26,7 @@ const QuestionnaireHero = () => {
               <input
                 type="text"
                 placeholder="Select Questionnaire Type"
-                className="md:mt-4 mt-2 px-5 lg:w-60  py-[8px] md:text-sm text-xs rounded-lg outline-none border-[1.5px] border-[#cdcdcd]"
+                className="md:mt-4 mt-2 px-5 lg:w-60 w-72  py-[8px] md:text-sm text-xs rounded-lg outline-none border-[1.5px] border-[#cdcdcd]"
               />
             </div>
           </div>
